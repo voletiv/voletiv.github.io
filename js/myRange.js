8939bf37e3579b3d66fcae423a8abd5f0455cc52
Vikram Voleti <vikram.voleti@gmail.com>
2017-01-16 19:57:31 +0530

    [MAJOR] added php code to read images

--- js/myRange.js
+++ js/myRange.js
@@ -1,3 +1,4 @@
+//http://stackoverflow.com/questions/11873570/angularjs-for-loop-with-numbers-ranges
 var myApp = angular.module('myApp', []);
 myApp.filter('range', function() {
   return function(input, total) {
